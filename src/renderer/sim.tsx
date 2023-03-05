48744708c4ec60069f339aa52a9ca220ebc3bc18
Bart van Heukelom <mail@bartvh.nl>
2023-03-05 17:01:34 +0100

    rotate boat

--- src/renderer/sim.tsx
+++ src/renderer/sim.tsx
@@ -21,11 +21,6 @@ export function runSim(canvas: HTMLCanvasElement, input: SimInput) {
     const scene = new Scene(engine);
 
 
-    const blackAlu = new StandardMaterial("blackAlu", scene);
-    blackAlu.diffuseColor = new Color3(0, 0, 0);
-    const navyBlueWood = new StandardMaterial("navyBlueWood", scene);
-    // navyBlueWood.diffuseColor = new Color3(0, 0, 0.5);
-    navyBlueWood.diffuseTexture = new Texture("https://www.babylonjs.com/assets/wood.jpg", scene);
     const waterMat = new StandardMaterial("water", scene);
     waterMat.diffuseColor = new Color3(0, 0, 1);
     waterMat.diffuseTexture = new Texture("https://www.babylonjs.com/assets/waterbump.png", scene);
@@ -46,6 +41,14 @@ export function runSim(canvas: HTMLCanvasElement, input: SimInput) {
     water.material = waterMat;
 
 
+    const blackAlu = new StandardMaterial("blackAlu", scene);
+    blackAlu.diffuseColor = new Color3(0, 0, 0);
+    const navyBlueWood = new StandardMaterial("navyBlueWood", scene);
+    // navyBlueWood.diffuseColor = new Color3(0, 0, 0.5);
+    navyBlueWood.diffuseTexture = new Texture("https://www.babylonjs.com/assets/wood.jpg", scene);
+    const offWhiteCloth = new StandardMaterial("offWhiteCloth", scene);
+    offWhiteCloth.diffuseColor = new Color3(1, 1, 0.9);
+
     const boat = new TransformNode("boat", scene);
     const body = MeshBuilder.CreateBox("body", {width: 1, height: 0.75, depth: 6}, scene);
     body.material = navyBlueWood;
@@ -64,6 +67,16 @@ export function runSim(canvas: HTMLCanvasElement, input: SimInput) {
     mast.position.y = (6 + 0.75) / 2;
     mast.position.z = 1;
     mast.parent = boat;
+
+    const sail = new TransformNode("sail", scene);
+    sail.parent =  mast;
+    sail.position.y = 2;
+
+    const sailMesh = MeshBuilder.CreateBox("sailMesh", {width: 0.01, height: 4, depth: 3}, scene);
+    sailMesh.parent = sail;
+    sailMesh.material = offWhiteCloth;
+    sailMesh.position.y = -2.5;
+    sailMesh.position.z = 1.6;
 
     const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 0, 0), scene);
     camera.attachControl(canvas, true);
@@ -87,6 +100,13 @@ export function runSim(canvas: HTMLCanvasElement, input: SimInput) {
         // boat.rotation.y += Math.cos(waRadians) * tiltAngle * a;
         boat.rotate(new Vector3(Math.sin(waRadians), 0, Math.cos(waRadians)), tiltAngle, Space.WORLD);
 
+        // bob bob bob
+        boat.rotate(new Vector3(1, 0, 0), Math.sin(t * 2) * Math.PI * 0.01, Space.WORLD);
+        boat.rotate(new Vector3(0, 0, 1), Math.sin(t * 1.5) * Math.PI * 0.01, Space.WORLD);
+        boat.rotate(new Vector3(0, 0, 1), Math.sin(t * 0.621) * Math.PI * 0.01, Space.WORLD);
+
+        sail.rotationQuaternion = null;
+        sail.rotate(new Vector3(0, 1, 0), waRadians - (Math.PI * 0.5), Space.LOCAL);
 
         scene.render();
         t += 1/60;
